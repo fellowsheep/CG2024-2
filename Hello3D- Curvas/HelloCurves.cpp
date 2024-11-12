e6cc8f953b67938f00b008b583729c33eb4d7013
Rossana Baptista Queiroz <fellowsheep@gmail.com>
2024-11-11 22:24:08 -0300

    Adicionada curva Catmul Rom

--- Hello3D- Curvas/HelloCurves.cpp
+++ Hello3D- Curvas/HelloCurves.cpp
@@ -24,18 +24,19 @@ using namespace std;
 // Classes utilitárias
 #include "Shader.h"
 
-struct BezierCurve {
+struct Curve {
     std::vector<glm::vec3> controlPoints; // Pontos de controle da curva
     std::vector<glm::vec3> curvePoints;   // Pontos da curva
-    glm::mat4x4 bernsteinMatrix;          // Matriz dos coeficientes de Bernstein
+    glm::mat4x4 M;          // Matriz dos coeficientes da curva
 };
 
 
 // Outras funções
 void initializeBernsteinMatrix(glm::mat4x4 &matrix);
-void generateCurvePoints(BezierCurve &curve, int numPoints);
-void setControlPoints(BezierCurve &curve, const std::vector<glm::vec3> &points);
-void displayBezierCurve(const BezierCurve &curve);
+void generateBezierCurvePoints(Curve &curve, int numPoints);
+void initializeCatmulRomMatrix(glm::mat4x4 &matrix);
+void generateCatmulRomCurvePoints(Curve &curve, int numPoints);
+void displayCurve(const Curve &curve);
 GLuint generateControlPointsBuffer(vector <glm::vec3> controlPoints);
 
 // Dimensões da janela (pode ser alterado em tempo de execução)
@@ -64,28 +65,38 @@ int main()
     Shader shader = Shader("./hello-curves.vs", "./hello-curves.fs");
 
     // Estrutura para armazenar a curva de Bézier e pontos de controle
-    BezierCurve curvaBezier;
+    Curve curvaBezier;
+    Curve curvaCatmulRom;
     curvaBezier.controlPoints = {
+    glm::vec3(-0.6, -0.4, 0.0),
     glm::vec3(-0.6, -0.4, 0.0),
 	glm::vec3(-0.4, -0.6, 0.0),
 	glm::vec3(-0.2, -0.2, 0.0),
 	glm::vec3(0.0, 0.0, 0.0),
 	glm::vec3(0.2, 0.2, 0.0),
 	glm::vec3(0.4, 0.6, 0.0),
+	glm::vec3(0.6, 0.4, 0.0),
 	glm::vec3(0.6, 0.4, 0.0)
     };
-
+    curvaCatmulRom.controlPoints = curvaBezier.controlPoints;
     //curvaBezier.controlPoints = { glm::vec3(-0.8f, -0.4f, 0.0f), glm::vec3(-0.4f, 0.4f, 0.0f),
     //                              glm::vec3(0.4f, 0.4f, 0.0f), glm::vec3(0.8f, -0.4f, 0.0f) };
 
     // Gerar pontos da curva de Bézier
-    int numCurvePoints = 100; // Quantidade de pontos na curva
-    generateCurvePoints(curvaBezier, numCurvePoints);
+    int numCurvePoints = 100; // Quantidade de pontos por segmento na curva
+    generateBezierCurvePoints(curvaBezier, numCurvePoints);
+    generateCatmulRomCurvePoints(curvaCatmulRom, numCurvePoints);
 
 
     //Cria os buffers de geometria dos pontos da curva
     GLuint VAOControl = generateControlPointsBuffer(curvaBezier.controlPoints);
-    GLuint VAOCurve = generateControlPointsBuffer(curvaBezier.curvePoints);
+    GLuint VAOBezierCurve = generateControlPointsBuffer(curvaBezier.curvePoints);
+    GLuint VAOCatmulRomCurve = generateControlPointsBuffer(curvaCatmulRom.curvePoints);
+
+    cout << curvaBezier.controlPoints.size() << endl;
+    cout << curvaBezier.curvePoints.size() << endl;
+    cout << curvaCatmulRom.curvePoints.size() << endl;
+
 
     shader.Use();
 
@@ -106,29 +117,35 @@ int main()
 
         // Desenhar pontos de controle maiores e com cor diferenciada
         glBindVertexArray(VAOControl);
-        shader.setVec3("finalColor", 1.0f, 0.0f, 0.0f); // Vermelho para pontos de controle
+        shader.setVec4("finalColor", 1.0f, 0.0f, 0.0f,1.0f); // Vermelho para pontos de controle
         glPointSize(8.0f);
         glDrawArrays(GL_POINTS, 0, curvaBezier.controlPoints.size());
         
-        // Desenhar pontos da curva e conectar com linhas
-        glBindVertexArray(VAOCurve);
-        shader.setVec3("finalColor", 0.0f, 0.0f, 1.0f); // Azul para a curva
-        glPointSize(4.0f);
+        // Desenhar pontos da curva de Bezier e conectar com linhas
+        glBindVertexArray(VAOBezierCurve);
+        shader.setVec4("finalColor", 0.0f, 0.0f, 1.0f,1.0f); // Azul para a curva
+        glLineWidth(10.0);
         glDrawArrays(GL_LINE_STRIP, 0, curvaBezier.curvePoints.size()); // Desenha a curva como uma linha contínua
       
+       // Desenhar pontos da curva de Bezier e conectar com linhas
+        glBindVertexArray(VAOCatmulRomCurve);
+        shader.setVec4("finalColor", 0.0f, 1.0f, 0.0f,1.0f); // Azul para a curva
+        glDrawArrays(GL_LINE_STRIP, 0, curvaCatmulRom.curvePoints.size()); // Desenha a curva como uma linha contínua
+
         // Troca os buffers da tela
         glfwSwapBuffers(window);
     }
     // Pede pra OpenGL desalocar os buffers
     glDeleteVertexArrays(1, &VAOControl);
-    glDeleteVertexArrays(1, &VAOCurve);
+    glDeleteVertexArrays(1, &VAOBezierCurve);
+    glDeleteVertexArrays(1, &VAOCatmulRomCurve);
     // Finaliza a execução da GLFW, limpando os recursos alocados por ela
     glfwTerminate();
 
     return 0;
 }
 
-void initializeBernsteinMatrix(glm::mat4x4 &matrix)
+void initializeBernsteinMatrix(glm::mat4 &matrix)
 {
     matrix[0] = glm::vec4(1.0f, -3.0f, 3.0f, -1.0f);
     matrix[1] = glm::vec4(0.0f, 3.0f, -6.0f, 3.0f);
@@ -136,17 +153,29 @@ void initializeBernsteinMatrix(glm::mat4x4 &matrix)
     matrix[3] = glm::vec4(0.0f, 0.0f, 0.0f, 1.0f);
 }
 
-void generateCurvePoints(BezierCurve &curve, int numPoints) {
+
+void initializeCatmulRomMatrix(glm::mat4 &matrix)
+{
+    matrix[0] = glm::vec4(-1.0f, 3.0f, -3.0f, 1.0f);
+    matrix[1] = glm::vec4(2.0f, -5.0f, 4.0f, -1.0f);
+    matrix[2] = glm::vec4(-1.0f, 0.0f, 1.0f, 0.0f);
+    matrix[3] = glm::vec4(0.0f, 2.0f, 0.0f, 0.0f);
+}
+
+void generateBezierCurvePoints(Curve &curve, int numPoints) {
     curve.curvePoints.clear(); // Limpa quaisquer pontos antigos da curva
 
-    initializeBernsteinMatrix(curve.bernsteinMatrix);
+    initializeBernsteinMatrix(curve.M);
     // Calcular os pontos ao longo da curva com base em Bernstein
      // Loop sobre os pontos de controle em grupos de 4
-    for (size_t i = 0; i < curve.controlPoints.size() - 3; i += 3) {
+
+    float piece = 1.0/ (float) numPoints;
+    float t;
+    for (int i = 0; i < curve.controlPoints.size() - 3; i += 3) {
         
         // Gera pontos para o segmento atual
-        for (int j = 0; j <= numPoints;j++) {
-            float t = static_cast<float>(j) / numPoints;
+        for (int j = 0; j < numPoints;j++) {
+            t = j * piece;
             
             // Vetor t para o polinômio de Bernstein
             glm::vec4 T(t * t * t, t * t, t, 1);
@@ -159,19 +188,47 @@ void generateCurvePoints(BezierCurve &curve, int numPoints) {
             );
 
             // Calcula o ponto da curva multiplicando tVector, a matriz de Bernstein e os pontos de controle
-            glm::vec3 point = G * curve.bernsteinMatrix * T;
+            glm::vec3 point = G * curve.M * T;
             
             curve.curvePoints.push_back(point);
         }
     }
 }
 
+void generateCatmulRomCurvePoints(Curve &curve, int numPoints) {
+    curve.curvePoints.clear(); // Limpa quaisquer pontos antigos da curva
 
-void setControlPoints(BezierCurve &curve, const std::vector<glm::vec3> &points)
-{
-    curve.controlPoints = points;
-    initializeBernsteinMatrix(curve.bernsteinMatrix);
+    initializeCatmulRomMatrix(curve.M);
+    // Calcular os pontos ao longo da curva com base em Bernstein
+     // Loop sobre os pontos de controle em grupos de 4
+
+    float piece = 1.0 / (float) numPoints;
+    float t;
+    for (int i = 0; i < curve.controlPoints.size()-3; i += 3) {
+        
+        // Gera pontos para o segmento atual
+        for (int j = 0; j < numPoints;j++) {
+            t = j * piece;
+            
+            // Vetor t para o polinômio de Bernstein
+            glm::vec4 T(t * t * t, t * t, t, 1);
+            
+            glm::vec3 P0 = curve.controlPoints[i];
+			glm::vec3 P1 = curve.controlPoints[i + 1];
+			glm::vec3 P2 = curve.controlPoints[i + 2];
+			glm::vec3 P3 = curve.controlPoints[i + 3];
+
+			glm::mat4x3 G(P0, P1, P2, P3);
+
+
+            // Calcula o ponto da curva multiplicando tVector, a matriz de Bernstein e os pontos de controle
+            glm::vec3 point = G * curve.M * T;
+            point = point * glm::vec3(0.5, 0.5, 0.5);
+            curve.curvePoints.push_back(point);
+        }
+    }
 }
+
 
 GLuint generateControlPointsBuffer(vector <glm::vec3> controlPoints)
 {
--- Hello3D- Curvas/hello-curves.fs
+++ Hello3D- Curvas/hello-curves.fs
@@ -1,4 +1,4 @@
-#version 450 core
+#version 400
 
 uniform vec4 finalColor;
 
